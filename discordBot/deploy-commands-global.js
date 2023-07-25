be5ede8ff897ecab8227141688277a1accfbc414
sodadrinker43 <joshuacgriffin02@gmail.com>
2023-07-25 13:00:01 +0100

    Implemented Full Functionality
    - Global+Guild command deploy
    - Framework complete

---
+++ discordBot/deploy-commands-global.js
@@ -0,0 +1,45 @@
+// Deploys command for one guild, used to develop and test commands before global deployment 
+const { REST, Routes } = require('discord.js');
+const { clientId, guildId, token } = require('./config.json');
+const fs = require('node:fs');
+const path = require('node:path');
+
+const commands = [];
+// Grab all command files from command directory
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+    //grab all command files from command directory earlier 
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'))
+
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if ('data' in command && 'execute' in command) {
+			commands.push(command.data.toJSON());
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+    }
+}
+
+// Construct instance of REST module
+const rest = new REST().setToken(token);
+// Deploy commands
+(async () => {
+    try {
+        console.log(`Started refreshing ${commands.length} apllication (/) commands.`);
+
+        // 'Put' method refreshes all commands in guild
+        const data = await rest.put(
+            Routes.applicationCommands(clientId),
+            { body: commands },
+        );
+
+        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+    } catch (error) {
+        console.error(error);
+    }
+})();
